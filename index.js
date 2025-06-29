5450fa0d37171ee67771bae8d8dda472bea4bb0a
laggingreflex <laggingreflex@gmail.com>
2025-06-29 16:37:10 +0530

    sync option for console tee file logging

--- index.js
+++ index.js
@@ -4,34 +4,37 @@ import { formatWithOptions } from "util";
 const originalStdoutWrite = process.stdout.write;
 const originalStderrWrite = process.stderr.write;
 
-const logFile = new File(process.argv[2] || process.env["CONSOLE_TEE"] || "output.log");
-const errFile = new File(process.argv[3] || process.env["CONSOLE_TEE_ERR"] || logFile.filename);
+const sync = !(
+  process.argv.includes("--no-sync") ||
+  String(process.env["CONSOLE_TEE_SYNC"]).toLowerCase === "false"
+);
+const logFile = new File(process.argv[2] || process.env["CONSOLE_TEE"] || "output.log", { sync });
+const errFile = new File(process.argv[3] || process.env["CONSOLE_TEE_ERR"] || logFile.filename, {
+  sync,
+});
 
 process.stdout.write = function writeTee() {
   originalStdoutWrite.apply(process.stdout, arguments);
-  originalStderrWrite.apply(process.stderr, arguments);
   logFile.write(arguments);
 };
 
 process.stderr.write = function writeTee() {
   originalStderrWrite.apply(process.stderr, arguments);
-  originalStdoutWrite.apply(process.stdout, arguments);
-  logFile.write(arguments);
+  errFile.write(arguments);
 };
 
-const simpleLoggers = ["log", "info", "warn", "error", "debug"];
-simpleLoggers.forEach((method) => {
-  const originalMethod = console[method];
+for (const method of ["log", "info", "warn", "error", "debug"]) {
+  const fn = console[method];
   console[method] = function consoleTee(...args) {
     const msg = formatWithOptions({ depth: 10 }, ...args) + "\n";
-    originalMethod.apply(console, args);
+    fn.apply(console, args);
     if (method === "error") {
       errFile.write(msg);
     } else {
       logFile.write(msg);
     }
   };
-});
+}
 
 if (logFile.filename === errFile.filename) {
   console.log("console-tee:", logFile.filename);
@@ -41,10 +44,11 @@ if (logFile.filename === errFile.filename) {
 }
 
 class File {
-  constructor(filename) {
+  constructor(filename, { sync = false } = {}) {
     this.filename = filename;
     this.fd = fs.openSync(this.filename, "a");
     this.file = createWriteStream(null, { fd: this.fd, flags: "a" });
+    this.forceSync = sync;
   }
   write(args) {
     if (!args?.length) return;
@@ -53,10 +57,8 @@ class File {
     } catch (error) {
       console.error("Error writing to file:", error.message);
     }
-    try {
-      fs.fsyncSync(this.fd); // blocks until write hits disk
-    } catch (error) {
-      console.error("Error syncing file:", error.message);
+    if (this.forceSync) {
+      this.sync();
     }
   }
   writeSync(args) {
@@ -66,6 +68,9 @@ class File {
     } catch (error) {
       console.error("Error writing to file:", error.message);
     }
+    this.sync();
+  }
+  sync() {
     try {
       fs.fsyncSync(this.fd); // blocks until write hits disk
     } catch (error) {
@@ -78,4 +83,8 @@ class File {
       .map((item) => formatWithOptions({ depth: 10 }, item))
       .join("");
   }
+  get size() {
+    return fs.fstatSync(this.fd).size;
+  }
+  noop() {}
 }
